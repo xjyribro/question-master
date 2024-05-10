c68fb32dd1cb56dec5deb6c19252460cc7371ed2
xjyribro <limcheekee.63@gmail.com>
2024-05-10 12:14:23 +0800

    enabled all questions

--- questions.js
+++ questions.js
@@ -1,12 +1,12 @@
 export const questions = [
     { question: 'Question 1?', answer: '1' },
     { question: 'Question 2?', answer: '2' },
-    // { question: 'Question 3?', answer: '3' },
-    // { question: 'Question 4?', answer: '4' },
-    // { question: 'Question 5?', answer: '5' },
-    // { question: 'Question 6?', answer: '6' },
-    // { question: 'Question 7?', answer: '7' },
-    // { question: 'Question 8?', answer: '8' },
-    // { question: 'Question 9?', answer: '9' },
-    // { question: 'Question 10?', answer: '10' }
+    { question: 'Question 3?', answer: '3' },
+    { question: 'Question 4?', answer: '4' },
+    { question: 'Question 5?', answer: '5' },
+    { question: 'Question 6?', answer: '6' },
+    { question: 'Question 7?', answer: '7' },
+    { question: 'Question 8?', answer: '8' },
+    { question: 'Question 9?', answer: '9' },
+    { question: 'Question 10?', answer: '10' }
 ]
