ebf0d39d819fcd667611401514b631f0b1f9863f
xjyribro <limcheekee.63@gmail.com>
2024-05-21 09:45:53 +0800

    new question structure

--- questions.js
+++ questions.js
@@ -1,12 +1,350 @@
+export const flexiableOperators = ["+", "x"]
+
 export const questions = [
-    { question: 'Question 1?', answer: '1' },
-    { question: 'Question 2?', answer: '2' },
-    { question: 'Question 3?', answer: '3' },
-    { question: 'Question 4?', answer: '4' },
-    { question: 'Question 5?', answer: '5' },
-    { question: 'Question 6?', answer: '6' },
-    { question: 'Question 7?', answer: '7' },
-    { question: 'Question 8?', answer: '8' },
-    { question: 'Question 9?', answer: '9' },
-    { question: 'Question 10?', answer: '10' }
+  {
+    prompt: "There are five players on a basketball team.",
+    subQuestions: [
+      {
+        question: "How many shirts do we need for four teams?",
+        working: [
+          {
+            operands: [5, 4],
+            operator: "x",
+            answer: 20
+          }
+        ],
+        answer: 20
+      },
+      {
+        question: "How many pairs of shoes do we need for six teams?",
+        working: [
+          {
+            operands: [5, 6],
+            operator: "x",
+            answer: 30
+          }
+        ],
+        answer: 30
+      }
+    ]
+  },
+  {
+    prompt: `There are twelve trees.\n½ of them are apple trees.\n¼ of them are pear trees.\nThe rest are orange trees.`,
+    subQuestions: [
+      {
+        question: "How many of the trees are apple trees?",
+        working: [
+          {
+            operands: [12, 2],
+            operator: "/",
+            answer: 6
+          }
+        ],
+        answer: 6
+      },
+      {
+        question: "How many pear trees are there?",
+        working: [
+          {
+            operands: [12, 4],
+            operator: "/",
+            answer: 3
+          }
+        ],
+        answer: 3
+      },
+      {
+        question: `How many of the trees are orange trees?\n(Hint: First add up the number of apple and pear trees)`,
+        working: [
+          {
+            operands: [3, 6],
+            operator: "+",
+            answer: 9
+          },
+          {
+            operands: [12, 9],
+            operator: "-",
+            answer: 3
+          }
+        ],
+        answer: 3
+      }
+    ]
+  },
+  {
+    prompt: `There are ten children. Each child is pulling a wagon.\nThere are five blocks in each wagon.\nEach child holds three kites.`,
+    subQuestions: [
+      {
+        question: "How many blocks are there altogether?",
+        working: [
+          {
+            operands: [10, 5],
+            operator: "x",
+            answer: 50
+          }
+        ],
+        answer: 50
+      },
+      {
+        question: "How many kites are there altogether?",
+        working: [
+          {
+            operands: [10, 3],
+            operator: "x",
+            answer: 30
+          }
+        ],
+        answer: 30
+      }
+    ]
+  },
+  {
+    prompt: `There are three girls and five boys. Each child has ten balloons.`,
+    subQuestions: [
+      {
+        question: "How many balloons do the girls have?",
+        working: [
+          {
+            operands: [3, 10],
+            operator: "x",
+            answer: 30
+          }
+        ],
+        answer: 30
+      },
+      {
+        question: "How many balloons do the boys have?",
+        working: [
+          {
+            operands: [5, 10],
+            operator: "x",
+            answer: 50
+          }
+        ],
+        answer: 50
+      },
+      {
+        question: `How many balloons are there in total?\n(Hint: Add them up!)`,
+        working: [
+          {
+            operands: [50, 30],
+            operator: "+",
+            answer: 80
+          }
+        ],
+        answer: 80
+      },
+      {
+        question: `If each child loses two balloons, how many balloons will be left?\n(Hint: First find out how many balloons are lost)`,
+        working: [
+          {
+            operands: [2, 16],
+            operator: "x",
+            answer: 16
+          },
+          {
+            operands: [80, 16],
+            operator: "-",
+            answer: 64
+          }
+        ],
+        answer: 64
+      },
+    ]
+  },
+  {
+    prompt: `Seven people are standing with their right hands in the air\nand their left hands hidden behind their backs.`,
+    subQuestions: [
+      {
+        question: "How many fingers do we see?",
+        working: [
+          {
+            operands: [5, 7],
+            operator: "x",
+            answer: 35
+          }
+        ],
+        answer: 35
+      },
+      {
+        question: "How many legs do we see?",
+        working: [
+          {
+            operands: [2, 7],
+            operator: "x",
+            answer: 14
+          }
+        ],
+        answer: 14
+      }
+    ]
+  },
+  {
+    prompt: `The parking lot has space for sixteen cars.\nThere are four empty spaces.`,
+    subQuestions: [
+      {
+        question: "How many cars are in the lot?",
+        working: [
+          {
+            operands: [16, 4],
+            operator: "-",
+            answer: 12
+          }
+        ],
+        answer: 12
+      },
+      {
+        question: `Four of the cars are red.\nThere are as many blue cars as red cars.\nThe rest of the cars are green.\nHow many cars are green? (Hint: First add the red and blue cars)`,
+        working: [
+          {
+            operands: [4, 4],
+            operator: "+",
+            answer: 8
+          },
+          {
+            operands: [16, 8],
+            operator: "-",
+            answer: 8
+          }
+        ],
+        answer: 8
+      }
+    ]
+  },
+  {
+    prompt: `Toy cars cost one dollar. Kites cost ten cents. Marbles cost one cent\n
+    Elizabeth bought 2 toy cars, 5 kites and 8 marbles.\n
+    Glen bought 1 toy car, 10 kites and 12 marbles.\n
+    Esther bought 3 toy cars, 15 kites and 20 marbles.\n
+    How much did each one spend (in dollars)?`,
+    subQuestions: [
+      {
+        question: "Elizabeth. (1st row: cars, 2nd row: kites, 3rd row: marbles, 4th row: total)",
+        working: [
+          {
+            operands: [1, 2],
+            operator: "x",
+            answer: 2
+          },
+          {
+            operands: [0.1, 5],
+            operator: "x",
+            answer: 0.5
+          },
+          {
+            operands: [0.01, 8],
+            operator: "-",
+            answer: 0.08
+          },
+          {
+            operands: [0.08, 0.5, 12],
+            operator: "+",
+            answer: 2.58
+          }
+        ],
+        answer: 2.58
+      },
+      {
+        question: "Glen. (1st row: cars, 2nd row: kites, 3rd row: marbles, 4th row: total)",
+        working: [
+          {
+            operands: [1, 1],
+            operator: "x",
+            answer: 1
+          },
+          {
+            operands: [0.1, 10],
+            operator: "x",
+            answer: 1
+          },
+          {
+            operands: [0.01, 12],
+            operator: "-",
+            answer: 0.12
+          },
+          {
+            operands: [1, 1, 0.12],
+            operator: "+",
+            answer: 2.12
+          }
+        ],
+        answer: 2.12
+      },
+      {
+        question: "Esther. (1st row: cars, 2nd row: kites, 3rd row: marbles, 4th row: total)",
+        working: [
+          {
+            operands: [1, 3],
+            operator: "x",
+            answer: 3
+          },
+          {
+            operands: [0.1, 15],
+            operator: "x",
+            answer: 1.50
+          },
+          {
+            operands: [0.01, 20],
+            operator: "-",
+            answer: 0.20
+          },
+          {
+            operands: [3, 1.50, 0.20],
+            operator: "+",
+            answer: 4.70
+          }
+        ],
+        answer: 4.70
+      },
+    ]
+  },
+  {
+    prompt: `Here are ten plants.\nThere are five flowers on each plant.\nTwo of the flowers are red and three are yellow.`,
+    subQuestions: [
+      {
+        question: "How many red flowers in total?",
+        working: [
+          {
+            operands: [10, 2],
+            operator: "x",
+            answer: 20
+          }
+        ],
+        answer: 20
+      },
+      {
+        question: `How many yellow flowers in total?`,
+        working: [
+          {
+            operands: [10, 3],
+            operator: "x",
+            answer: 30
+          }
+        ],
+        answer: 30
+      },
+      {
+        question: `How many flowers are there in all?`,
+        working: [
+          {
+            operands: [30, 20],
+            operator: "+",
+            answer: 50
+          }
+        ],
+        answer: 50
+      },
+      {
+        question: `How many more plants would we need to get one hundred flowers?`,
+        working: [
+          {
+            operands: [100, 50],
+            operator: "-",
+            answer: 50
+          }
+        ],
+        answer: 50
+      }
+    ]
+  }
 ]
--- scripts.js
+++ scripts.js
@@ -1,64 +1,241 @@
-import { questions } from "./questions.js"
+import { flexiableOperators, questions } from "./questions.js"
+
+const FIRST = "first"
+const SECOND = "second"
+const OPERAND = "operand"
+const ANSWER = "answer"
+const SUBANSWER = "subAnswer"
+const COMMENT = "comment"
+const HEADING = "heading"
 
 document.addEventListener("DOMContentLoaded", function () {
   const mainSection = document.querySelector("main")
-  const questionText = document.createElement("p")
-  const inputBox = document.createElement("input")
-  inputBox.setAttribute("type", "text")
-  inputBox.placeholder = "Type your answer here"
-  const comment = document.createElement("p")
-  let currentIndex = 0
+  const promptText = document.createElement("p")
+  const header = document.getElementById(HEADING)
 
-  const button = document.createElement("button")
+  let currentQuestionIndex = 0
+  let currentSubQuestionIndex = 0
+
+  const sqrWidth = "20px"
+
+  let button = document.createElement("button")
   button.textContent = "Click here to start"
   button.addEventListener("click", startQuiz)
   mainSection.appendChild(button)
 
   function startQuiz() {
-    currentIndex = 0
-    questionText.textContent = questions[currentIndex]["question"]
+    header.remove()
     button.textContent = "Check answer"
-    inputBox.value = ""
-    button.removeEventListener("click", startQuiz)
-    button.addEventListener("click", checkAnswer)
-    mainSection.insertBefore(questionText, button)
-    mainSection.insertBefore(inputBox, button)
+    startQuestion()
+  }
+
+  function startQuestion() {
+    promptText.textContent = questions[currentQuestionIndex]["prompt"]
+    mainSection.insertBefore(promptText, button)
+    showSubQuestion()
+  }
+
+  function showSubQuestion() {
+    const subQ =
+      questions[currentQuestionIndex]["subQuestions"][currentSubQuestionIndex]
+    const subQuestionText = document.createElement("p")
+    subQuestionText.textContent = subQ.question
+
+    const finalAnswerinput = document.createElement("input")
+    const comment = document.createElement("p")
+    comment.setAttribute("id", COMMENT)
+    comment.style.fontWeight = "600"
+    finalAnswerinput.setAttribute("type", "text")
+    finalAnswerinput.setAttribute(
+      "id",
+      SUBANSWER + currentSubQuestionIndex.toString()
+    )
+    finalAnswerinput.placeholder = "Final answer"
+
+    mainSection.insertBefore(subQuestionText, button)
+
+    subQ.working.forEach((_, i) => {
+      const workingRow = document.createElement("div")
+      const firstOperandInput = document.createElement("input")
+      const secondOperandInput = document.createElement("input")
+      const operatorInput = document.createElement("input")
+      const answerInput = document.createElement("input")
+      firstOperandInput.setAttribute("type", "text")
+      secondOperandInput.setAttribute("type", "text")
+      operatorInput.setAttribute("type", "text")
+      answerInput.setAttribute("type", "text")
+      firstOperandInput.setAttribute("id", FIRST + i.toString())
+      secondOperandInput.setAttribute("id", SECOND + i.toString())
+      operatorInput.setAttribute("id", OPERAND + i.toString())
+      answerInput.setAttribute("id", ANSWER + i.toString())
+      firstOperandInput.style.width = sqrWidth
+      secondOperandInput.style.width = sqrWidth
+      operatorInput.style.width = sqrWidth
+      answerInput.style.width = sqrWidth
+
+      workingRow.appendChild(firstOperandInput)
+      workingRow.appendChild(operatorInput)
+      workingRow.appendChild(secondOperandInput)
+      workingRow.append("=")
+      workingRow.appendChild(answerInput)
+      mainSection.insertBefore(workingRow, button)
+    })
+    mainSection.insertBefore(finalAnswerinput, button)
+    mainSection.insertBefore(comment, button)
+    const newButton = button.cloneNode(true)
+    button.parentNode.replaceChild(newButton, button)
+    newButton.addEventListener("click", checkAnswer)
+    button = newButton
+    button.textContent = "Check answer"
+  }
+
+  function checkWorkingCorrect(
+    working,
+    firstInput,
+    secondInput,
+    operatorInput,
+    answerInput
+  ) {
+    const operator = working["operator"]
+    const operands = JSON.parse(JSON.stringify(working["operands"]))
+    const answer = working["answer"].toString()
+    const inputIsFlexible = flexiableOperators.includes(operator)
+
+    let isCorrect = true
+    if (inputIsFlexible) {
+      const index = operands.indexOf(parseInt(firstInput))
+      if (index < 0) {
+        isCorrect = false
+      } else {
+        operands.splice(index, 1)
+      }
+      isCorrect = isCorrect && operands.includes(parseInt(secondInput))
+    } else {
+      isCorrect =
+        operands[0].toString() === firstInput &&
+        operands[1].toString() === secondInput
+    }
+    isCorrect =
+      isCorrect && operator === operatorInput && answer === answerInput
+
+    return isCorrect
   }
 
   function checkAnswer() {
-    const userInput = inputBox.value.trim()
-    const correctAnswer = questions[currentIndex]["answer"]
-    if (!mainSection.contains(comment)) {
-      mainSection.insertBefore(comment, button)
+    const subQs = questions[currentQuestionIndex]["subQuestions"]
+    const subQ = subQs[currentSubQuestionIndex]
+    const workings = subQ.working
+    const ans = subQ.answer
+
+    const answerInput = document.getElementById(
+      SUBANSWER + currentSubQuestionIndex.toString()
+    )
+    const comment = document.getElementById(COMMENT)
+
+    let isFilled = workings.every((_, i) => {
+      const firstInput = document
+        .getElementById(FIRST + i.toString())
+        .value.trim()
+      const secondInput = document
+        .getElementById(SECOND + i.toString())
+        .value.trim()
+      const operatorInput = document
+        .getElementById(OPERAND + i.toString())
+        .value.trim()
+      const answerInput = document
+        .getElementById(ANSWER + i.toString())
+        .value.trim()
+
+      return (
+        firstInput !== "" &&
+        secondInput !== "" &&
+        operatorInput !== "" &&
+        answerInput !== ""
+      )
+    })
+
+    isFilled = isFilled && answerInput !== ""
+
+    if (!isFilled) {
+      comment.textContent = "Please fill all boxes"
+      comment.style.color = "red"
+      return
     }
-    if (userInput == correctAnswer) {
-      inputBox.remove()
-      if (currentIndex == questions.length - 1) {
-        comment.remove()
-        button.removeEventListener("click", nextQuestion)
-        button.addEventListener("click", startQuiz)
-        button.textContent = "Reset quiz"
-        questionText.textContent =
-          "Congratulations!! you have answered all questions correctly!"
-      } else {
-        comment.textContent = "Correct!"
-        button.removeEventListener("click", checkAnswer)
+
+    let isCorrect = workings.every((working, i) => {
+      const firstInput = document
+        .getElementById(FIRST + i.toString())
+        .value.trim()
+      const secondInput = document
+        .getElementById(SECOND + i.toString())
+        .value.trim()
+      const operatorInput = document
+        .getElementById(OPERAND + i.toString())
+        .value.trim()
+      const answerInput = document
+        .getElementById(ANSWER + i.toString())
+        .value.trim()
+
+      return checkWorkingCorrect(
+        working,
+        firstInput,
+        secondInput,
+        operatorInput,
+        answerInput
+      )
+    })
+    const answerValue = answerInput.value
+    isCorrect = isCorrect && answerValue == ans
+
+    if (isCorrect) {
+      comment.textContent = "Correct!"
+      comment.style.color = "green"
+      button.removeEventListener("click", checkAnswer)
+      if (subQs.length - 1 === currentSubQuestionIndex) {
         button.addEventListener("click", nextQuestion)
-        button.textContent = "Go to next question"
+        button.textContent = "Next Question"
+      } else {
+        button.addEventListener("click", nextSubQuestion)
+        button.textContent = "Continue"
       }
     } else {
       comment.textContent = "Wrong!"
+      comment.style.color = "red"
     }
   }
 
-  function nextQuestion() {
-    currentIndex++
+  function nextSubQuestion() {
+    const comment = document.getElementById(COMMENT)
     comment.remove()
-    button.removeEventListener("click", nextQuestion)
-    inputBox.value = ""
-    mainSection.insertBefore(inputBox, button)
-    questionText.textContent = questions[currentIndex]["question"]
-    button.addEventListener("click", checkAnswer)
-    button.textContent = "Check answer"
+    currentSubQuestionIndex++
+    showSubQuestion()
+  }
+
+  function completeQuiz() {
+    currentQuestionIndex = 0
+    header.textContent = "Congratulations!"
+    mainSection.insertBefore(header, button)
+    const newButton = button.cloneNode(true)
+    button.parentNode.replaceChild(newButton, button)
+    newButton.addEventListener("click", startQuiz)
+    button = newButton
+
+  }
+
+  function nextQuestion() {
+    const mainSection = document.querySelector("main")
+    const children = Array.from(mainSection.children)
+    children.forEach((child) => {
+      if (child.tagName.toLowerCase() !== "button") {
+        mainSection.removeChild(child)
+      }
+    })
+    currentQuestionIndex++
+    currentSubQuestionIndex = 0
+    if (currentQuestionIndex === questions.length) {
+      completeQuiz()
+    } else {
+      startQuestion()
+    }
   }
 })
